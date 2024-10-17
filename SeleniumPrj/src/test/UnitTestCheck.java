9c4890f6f3f75227042886184924e5de2c6d42a7
Sreenath S <cybersree2001@gmail.com>
2024-10-17 17:05:20 +0530

    Added file UnitTesdtCheck

---
+++ SeleniumPrj/src/test/UnitTestCheck.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.junit.Test;
+public class UnitTestCheck {
+	
+	WebDriver driver;
+	@Test
+	public void runTest()
+	{
+		System.out.print("Verify the string....");
+	}
+
+}
