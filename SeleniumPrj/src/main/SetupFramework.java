7144b40c03ec160e293dfa4040edc3fb73b8d2e5
Sreenath S <cybersree2001@gmail.com>
2024-10-17 08:54:56 +0530

    Resolved conflicts for project Seleniumprj

--- SeleniumPrj/src/main/SetupFramework.java
+++ SeleniumPrj/src/main/SetupFramework.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main;
 
 import org.openqa.selenium.By;
@@ -46,52 +45,4 @@ public class SetupFramework {
 		action.click().build().perform();
 	}
 }
-=======
-package main;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-
-public class SetupFramework {
-	public static WebDriver driver;
-
-	public SetupFramework() {
-		
-	}
-
-	public WebDriver setupBrowser() throws InterruptedException {
-
-		
-		 System.setProperty( "webdriver.chrome.driver",
-		 "C:\\Users\\Sreenath\\Documents\\jar_files\\chromedriver.exe"); //
-		  //Instantiate a ChromeDriver class. driver = new ChromeDriver();
-		 driver = new ChromeDriver();
-		// Maximize the browser
-		driver.manage().window().maximize();
-
-		// Launch Website
-		driver.get("https://www.geeksforgeeks.org/");
-		return driver;
-		// driver.wait(5);
-
-	}
-
-	public void clickMenu() throws InterruptedException {
-		WebElement Ele = driver.findElement(By.xpath("//li/span[text()='Courses']"));
-		Actions action = new Actions(driver);
-		action.moveToElement(Ele).perform();
-		Thread.sleep(5);
-	}
-
-	public void clickSubMenu() {
-		WebElement ele = driver.findElement(By.xpath("//li//a[text()='DSA to Development']"));
-		Actions action = new Actions(driver);
-		action.moveToElement(ele);
-		action.click().build().perform();
-	}
-}
->>>>>>> 236e11862dfbfe082633c57ea90abd285e540bbc
--- SeleniumPrj/src/test/componentTest.java
+++ SeleniumPrj/src/test/componentTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package test;
 
 
@@ -12,7 +11,7 @@ import component.SetupBrowser;
 import org.junit.Test;
 import main.SetupFramework;
 
-public class componentTest {
+public class componentTest extends SetupBrowser{
 	WebDriver driver;
 	@Test
 	public void verifyMenu() throws InterruptedException
@@ -31,37 +30,3 @@ public class componentTest {
 
 }
 
-=======
-package test;
-
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import component.SetupBrowser;
-
-import org.junit.Test;
-import main.SetupFramework;
-
-public class componentTest {
-	WebDriver driver;
-	@Test
-	public void verifyMenu() throws InterruptedException
-	{
-		SetupBrowser sub = new SetupBrowser();
-		sub.createComponent();
-		//WebElement ele= ChromeDriver.findElement(By.xpath("//div/h1/div[text()='DSA to Development:']"));
-		
-		
-		/*
-		 * SetupFramework suf = new SetupFramework(); suf.setupBrowser();
-		 * suf.clickMenu(); suf.clickSubMenu();
-		 */
-		
-	}
-
-}
-
->>>>>>> 236e11862dfbfe082633c57ea90abd285e540bbc
